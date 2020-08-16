2ab1351c7c84a6dfd73e646ac8d4e35634f7e828
袁智远 <1262163090@qq.com>
2020-08-16 11:23:00 +0800

    删除json.net序列化

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -81,8 +81,7 @@ namespace Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers()
-                .AddNewtonsoftJson();
+            services.AddControllers();
 
             ConfigAuth(services);
             ConfigSwagger(services);
